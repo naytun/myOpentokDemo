147080b6c4e1253223b97baced65373e81bf6211
nay tun thein <naytunthein70@gmail.com>
2018-02-26 13:45:36 -0500

    Upload mydemo OpenTok web app

---
+++ src/test-chrome-firefox-safari-ie.js
@@ -0,0 +1,32 @@
+/* global browser describe it beforeAll $ */
+
+const assert = require("assert");
+
+describe("Basic Video Chat Test", () => {
+  beforeAll(() => {
+    browser.url("Basic Video Chat");
+  });
+
+  it("should have the right title", () => {
+    const title = browser.getTitle();
+    assert.equal(title, "OpenTok Getting Started");
+  });
+
+  it("The publisher should load", () => {
+    const publisher = $("div.OT_publisher:not(.OT_loading) .OT_video-element");
+    publisher.waitForExist(15000);
+  });
+
+  it("The subscriber should load if you open a new window", () => {
+    browser.newWindow(".");
+    const subscriber = $(
+      "div.OT_subscriber:not(.OT_loading) .OT_video-element"
+    );
+    subscriber.waitForExist(15000);
+    browser.switchTab();
+    const subscriber2 = $(
+      "div.OT_subscriber:not(.OT_loading) .OT_video-element"
+    );
+    subscriber2.waitForExist(15000);
+  });
+});

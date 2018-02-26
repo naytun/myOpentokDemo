057845738ad1efffa5d42f5ea9274463fcfa9b5a
nay tun thein <naytunthein70@gmail.com>
2018-02-26 13:58:13 -0500

    .

--- src/app.js
+++ src/app.js
@@ -15,25 +15,30 @@ function initializeSession() {
   var session = OT.initSession(apiKey, sessionId);
 
   // Subscribe to a newly created stream
-  session.on('streamCreated', function streamCreated(event) {
+  session.on("streamCreated", function streamCreated(event) {
     var subscriberOptions = {
-      insertMode: 'append',
-      width: '100%',
-      height: '100%'
+      insertMode: "append",
+      width: "100%",
+      height: "100%"
     };
-    session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+    session.subscribe(
+      event.stream,
+      "subscriber",
+      subscriberOptions,
+      handleError
+    );
   });
 
-  session.on('sessionDisconnected', function sessionDisconnected(event) {
-    console.log('You were disconnected from the session.', event.reason);
+  session.on("sessionDisconnected", function sessionDisconnected(event) {
+    console.log("You were disconnected from the session.", event.reason);
   });
 
   var publisherOptions = {
-    insertMode: 'append',
-    width: '100%',
-    height: '100%'
+    insertMode: "append",
+    width: "100%",
+    height: "100%"
   };
-  var publisher = OT.initPublisher('publisher', publisherOptions, handleError);
+  var publisher = OT.initPublisher("publisher", publisherOptions, handleError);
 
   // Connect to the session
   session.connect(token, function callback(error) {
@@ -54,16 +59,21 @@ if (API_KEY && TOKEN && SESSION_ID) {
   initializeSession();
 } else if (SAMPLE_SERVER_BASE_URL) {
   // Make an Ajax request to get the OpenTok API key, session ID, and token from the server
-  fetch(SAMPLE_SERVER_BASE_URL + '/session').then(function fetch(res) {
-    return res.json();
-  }).then(function fetchJson(json) {
-    apiKey = json.apiKey;
-    sessionId = json.sessionId;
-    token = json.token;
+  fetch(SAMPLE_SERVER_BASE_URL + "/room/session")
+    .then(function fetch(res) {
+      return res.json();
+    })
+    .then(function fetchJson(json) {
+      apiKey = json.apiKey;
+      sessionId = json.sessionId;
+      token = json.token;
 
-    initializeSession();
-  }).catch(function catchErr(error) {
-    handleError(error);
-    alert('Failed to get opentok sessionId and token. Make sure you have updated the config.js file.');
-  });
+      initializeSession();
+    })
+    .catch(function catchErr(error) {
+      handleError(error);
+      alert(
+        "Failed to get opentok sessionId and token. Make sure you have updated the config.js file."
+      );
+    });
 }
